2990cc90d3212799f38e37795ab86ef65a53456d
axin <truongaxin123@gmail.com>
2022-09-13 22:38:08 +0700

    add model

---
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BookStoreApp.API.Data
+{
+    public partial class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext()
+        {
+        }
+
+        public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Author> Authors { get; set; } = null!;
+        public virtual DbSet<Book> Books { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=localhost\\sqlexpress;Database=BookStoreDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Author>(entity =>
+            {
+                entity.Property(e => e.Bio).HasMaxLength(250);
+
+                entity.Property(e => e.FirstName).HasMaxLength(50);
+
+                entity.Property(e => e.LastName).HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<Book>(entity =>
+            {
+                entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EA78C4809F")
+                    .IsUnique();
+
+                entity.Property(e => e.Image).HasMaxLength(50);
+
+                entity.Property(e => e.Isbn)
+                    .HasMaxLength(50)
+                    .HasColumnName("ISBN");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Summary).HasMaxLength(50);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+
+                entity.HasOne(d => d.Author)
+                    .WithMany(p => p.Books)
+                    .HasForeignKey(d => d.AuthorId)
+                    .HasConstraintName("FK_Books_ToTable");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
